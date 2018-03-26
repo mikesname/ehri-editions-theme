939d238d15a14c42815572f9724358d9e8aa3b73
trivette72 <36375587+trivette72@users.noreply.github.com>
2018-03-26 20:19:55 +0200

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -6,9 +6,9 @@
 
     <div id="footer-content" class="center-div">      
         <p>
-			<img class="footer-logo" src="https://editionstest.ehri-project-stage.eu/themes/ehri/images/logo-bottom-ehri.png" title="EHRI">
-			<img class="footer-logo" src="https://editionstest.ehri-project-stage.eu/themes/ehri/images/logo-bottom-na1.png" title="NA1">
-			<img class="footer-logo" src="https://editionstest.ehri-project-stage.eu/themes/ehri/images/logo-bottom-na2.png" title="NA2">
+			<img class="footer-logo" src="<?php echo WEB_ROOT . "/themes/ehri/images/logo-bottom-ehri.png" ?>" title="EHRI">
+			<img class="footer-logo" src="<?php echo WEB_ROOT . "/themes/ehri/images/logo-bottom-na1.png" ?>" title="NA1">
+			<img class="footer-logo" src="<?php echo WEB_ROOT . "/themes/ehri/images/logo-bottom-na2.png" ?>" title="NA2">
 	    </p>
 	    <?php if($footerText = get_theme_option('Footer Text')): ?>
             <p><?php echo get_theme_option('Footer Text'); ?></p>
@@ -24,7 +24,7 @@
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5aa59d527f6db3d6"></script>
 
 <!-- slick -->
-<script type="text/javascript" src="https://editionstest.ehri-project-stage.eu/themes/ehri/slick/slick.js"></script>
+<script type="text/javascript" src="<?php echo WEB_ROOT . "/themes/ehri/slick/slick.js" ?>"></script>
 <script type="text/javascript">
   $(document).ready(function(){
   $('.featured-carousel').slick({
