c14baeedfc395537f53504dbeab2ecbbc9371b3c
trivette72 <36375587+trivette72@users.noreply.github.com>
2018-03-28 19:26:17 +0200

    Add files via upload

--- solr-search/results/index.php
+++ solr-search/results/index.php
@@ -7,88 +7,88 @@
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
 
-$searchQuery =array_key_exists('q', $_GET) ? $_GET['q'] : '';
+$searchQuery = array_key_exists('q', $_GET) ? $_GET['q'] : '';
 ?>
 
-<?php echo head(array('title' => __('Solr Search'), 'results' => $results));?>
+<?php echo head(array('title' => __('Solr Search'), 'results' => $results, 'bodyclass' => 'search'));?>
 
+<?php if (isset($_GET["q"]) and trim($_GET["q"]) !== "" and ($results->response->numFound)>0): ?>
+    <h1><?php echo __("Search Results for \"%s\": %d", $searchQuery, $results->response->numFound); ?></h1>
 
-<?php if (isset($_GET["q"]) and trim($_GET["q"]) !== ""): ?>
-    <h1><?php echo __("Search Results for \"%s\": %d", $_GET['q'], $results->response->numFound); ?></h1>
+	<?php echo pagination_links(); ?>
+
+	<!-- Results. -->
+	<div class="search-results">
+
+	  <!-- Number found. -->
+		<?php $db = get_db(); ?>
+		<?php foreach ($results->response->docs as $doc): ?>
+
+			<?php $record = $db->getTable("Item")->find($doc->modelid); ?>
+			<?php $url = SolrSearch_Helpers_Index::getUri($record); ?>
+
+			<?php set_current_record("item", $record); ?>
+
+			<div class="search-result">
+				<?php
+					$date = metadata($record, array('Dublin Core', 'Date'));
+					$place = metadata($record, array('Dublin Core', 'Publisher'));
+				?>
+				<a href="<?php echo $url;?>">
+					<?php if ($recordImage = record_image("item")):  ?>
+						<?php echo $recordImage ?>
+					<?php else: ?>
+						<div class="search-result-image-blank"></div>
+					<?php endif; ?>
+				</a>
+				<div class="search-result-wrapper">
+					<a class="search-result-link" href="<?php echo $url; ?>">
+						<div class="search-result-title"><?php echo metadata($record, 'display_title'); ?></div>
+					</a>
+					<?php if (isset($date)){ $addition = $date; } ?>
+					<?php if (isset($place)){ $addition = $date . " | " . $place; }?>
+					<?php if($addition){echo "<p>$addition</p>";} ?>
+					<p><?php echo metadata($record, array('Dublin Core', 'Description'), array('snippet'=>300)); ?></p>
+					<p>Languages: <?php echo metadata($record, array('Dublin Core', 'Language')); ?></p>
+				</div>
+			</div>
+		<?php endforeach; ?>
+	</div>
+
+	<?php echo pagination_links(); ?>
+
 <?php else: ?>
-    <h1><?php echo __('Search Results'); ?></h1>
+	<div id="no-results" class="search-result-empty"><h1>
+	<?php if (strlen($searchQuery)>0) { ?>
+		No results for “<?php echo $searchQuery; ?>”.
+	<?php } else { ?>
+		Search query is empty.
+	<?php } ?>
+	</h1></div>
 <?php endif; ?>
 
-<?php echo pagination_links(); ?>
-
-<!-- Results. -->
-<div class="search-results">
-
-  <!-- Number found. -->
-  <h2 id="num-found">
-    <?php echo $results->response->numFound; ?> results
-  </h2>
-
-    <?php $db = get_db(); ?>
-    <?php foreach ($results->response->docs as $doc): ?>
-
-        <?php $record = $db->getTable("Item")->find($doc->modelid); ?>
-        <?php $url = SolrSearch_Helpers_Index::getUri($record); ?>
-
-        <?php set_current_record("item", $record); ?>
-
-        <div class="search-result">
-            <?php
-                $date = metadata($record, array('Dublin Core', 'Date'));
-                $place = metadata($record, array('Dublin Core', 'Publisher'));
-            ?>
-            <a href="<?php echo $url;?>">
-                <?php if ($recordImage = record_image("item")):  ?>
-                    <?php echo $recordImage ?>
-                <?php else: ?>
-                    <div class="search-result-image-blank"></div>
-                <?php endif; ?>
-            </a>
-            <div class="search-result-wrapper">
-                <a class="search-result-link" href="<?php echo $url; ?>">
-                    <div class="search-result-title"><?php echo metadata($record, 'display_title'); ?></div>
-                </a>
-                <?php if (isset($date)){ $addition = $date; } ?>
-                <?php if (isset($place)){ $addition = $date . " | " . $place; }?>
-                <?php if($addition){echo "<p>$addition</p>";} ?>
-                <p><?php echo metadata($record, array('Dublin Core', 'Description'), array('snippet'=>300)); ?></p>
-                <p>Languages: <?php echo metadata($record, array('Dublin Core', 'Language')); ?></p>
-            </div>
-        </div>
-    <?php endforeach; ?>
-</div>
-
-<?php echo pagination_links(); ?>
-
 <script>
-jQuery(document).ready(function($){
-	if ($(window).width() > 930) {
-		$("#nav-bar-search").show( 0, function() {});
-		$("#nav-bar-button-search").attr('class', 'nav-bar-button-search-selected');
-		if ($(window).width() < 1180) {
-			$("#nav-bar-search").attr('class', 'nav-bar-search-shadow');
-			setTimeout(function(){
-				$( "#nav-bar-search" ).hide( "slide", 1000, function() {});
-				$("#nav-bar-button-search" ).attr('class', 'nav-bar-button-search');
-			}, 700);
-		} else {
-			$("#container").animate({ 'margin-left': '240px'}, 0);
-			$("#footer").animate({ 'margin-left': '240px' }, 0);
-			$("#nav-bar-search").show( 0, function() {});
-			$("#nav-bar-button-search").attr('class', 'nav-bar-button-search-selected');
-		}
+<!-- show sidebar -->
+if ($(window).width() < 930) {
+	$("#nav-bar-search-mobile").show( 0, function() {});
+	$("#nav-bar-button-search").attr('class', 'nav-bar-button-search');
+} else {
+	$("#nav-bar-mobile-icon-search").text('close');
+	$("#nav-bar-mobile-button-search").attr('class', 'nav-bar-mobile-button-search-selected');
+	$("#nav-bar-search").show( 0, function() {});
+	$("#container").animate({ 'margin-left': '240px'}, 0);
+	$("#footer").animate({ 'margin-left': '240px' }, 0);
+	$("#nav-bar-limit-search").css('display', 'none');
+	$("#nav-bar-limit-shrink").css('display', 'none');
+	$("#nav-bar-limit-expand").css('display', 'inline');
+}
+
+<!-- hide logo -->
+$( window ).resize(function() {
+	if ($(window).width() < 1000) {
+		$("#site-logo").hide( 0, function() {});
 	} else {
-		$("#nav-bar-mobile-icon-search").text('close');
-		$("#nav-bar-mobile-button-search").attr('class', 'nav-bar-mobile-button-search-selected');
-		$("#nav-bar-search").show( 0, function() {});
-		$("#nav-bar-limit-search").css('display', 'none');
-		$("#nav-bar-limit-shrink").css('display', 'none');
-		$("#nav-bar-limit-expand").css('display', 'inline');
+		$("#site-logo").show( 0, function() {});
 	}
 });
 </script>
