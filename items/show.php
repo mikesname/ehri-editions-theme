4ffc7204d08af190265e5663fd98daf8880421d8
Mike Bryant <mikesname@gmail.com>
2018-04-09 11:09:58 +0100

    Implement working PDF/EPUB download links

--- items/show.php
+++ items/show.php
@@ -18,10 +18,21 @@ jQuery(function($) {
 </script>
 
 <div id="content-share">
-    <div id="content-share-title"><?php echo __('Download'); ?></div> 
-		<a id="pdf-create" class="content-share-item"><div class="content-share-item-icon">insert_drive_file</div><?php echo __('PDF'); ?></a>
-		<a class="content-share-item"><div class="content-share-item-icon">insert_drive_file</div><?php echo __('E-pub'); ?></a>
-	<div id="content-share-subtitle"><?php echo __('Share'); ?></div>
+    <?php $pdf = tei_editions_get_first_file_with_extension($item, ".pdf"); ?>
+    <?php $epub = tei_editions_get_first_file_with_extension($item, ".epub"); ?>
+
+    <?php if ($pdf or $epub): ?>
+    <div id="content-share-title">Download</div>
+        <?php if ($pdf): ?>
+		    <a href="<?php echo $pdf->getWebPath(); ?>"
+               id="pdf-create" class="content-share-item"><div class="content-share-item-icon">insert_drive_file</div>PDF</a>
+        <?php endif; ?>
+        <?php if ($epub): ?>
+		    <a href="<?php echo $epub->getWebPath(); ?>"
+               class="content-share-item"><div class="content-share-item-icon">insert_drive_file</div>E-pub</a>
+        <?php endif; ?>
+    <?php endif; ?>
+    <div id="content-share-subtitle"><?php echo __('Share'); ?></div>
 	<div class="addthis_inline_share_toolbox"></div>
     <?php if (plugin_is_active('Feedback')): ?>
 	<div id="feedback">
