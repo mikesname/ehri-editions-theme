f6d4d59c5568b6c1f6bc2a758c3d89c875757fe9
Mike Bryant <mikesname@gmail.com>
2018-05-04 13:05:49 +0100

    Fix the feedback functionality and add a thanks popover
    This now relies on a simple plugin called 'Feedback' which handles
    the form contents, stores the feedback in the DB, and sends a
    notification.

--- items/show.php
+++ items/show.php
@@ -23,27 +23,29 @@ jQuery(function($) {
 		<a class="content-share-item"><div class="content-share-item-icon">insert_drive_file</div><?php echo __('E-pub'); ?></a>
 	<div id="content-share-subtitle"><?php echo __('Share'); ?></div>
 	<div class="addthis_inline_share_toolbox"></div>
+    <?php if (plugin_is_active('Feedback')): ?>
 	<div id="feedback">
 		<div id="feedback-form" style='display:none;' class="col-xs-4 col-md-4 panel panel-default">
 			<h5><?php echo __('Feedback'); ?></h5>
 			<div id="feedback-close" class="material-icons">close</div>
-			<form enctype="application/x-www-form-urlencoded" method="post" action="<?php echo WEB_ROOT . "/editions/feedback/send"; ?>" class="form panel-body" role="form">
+			<form enctype="application/x-www-form-urlencoded" method="post" action="<?php echo $this->url('feedback/public/send'); ?>" class="form panel-body" role="form">
 				<div class="form-group">
 					<input class="form-control" name="email" id="email" autofocus placeholder="<?php echo __('Your e-mail'); ?>" type="email" />
-					<input class="form-control" name="username" id="username" type="text" placeholder="Username" value=""></dd>
-					<input type="hidden" name="timestamp" id="timestamp" value="<?php echo time(); ?>">
+					<input class="form-control" name="username" id="username" type="text" placeholder="Username" value="">
+					<input type="hidden" name="timestamp" id="timestamp" value="<?php echo (new DateTime())->format('Y-m-d H:i:s'); ?>">
 				</div>
 				<div class="form-group">
 					<textarea class="form-control" name="feedback" id="feedback" placeholder="<?php echo __('Please write your feedback here...'); ?>" rows="8"></textarea>
 				</div>
-					<input class="form-control" name="url" id="url" type="hidden" value="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']; ?>" />
-					<input class="form-control" name="title" id="title" type="hidden" value="<?php echo option('site_title'); ?>" />
+                <input class="form-control" name="url" id="url" type="hidden" value="<?php echo absolute_url(); ?>" />
+                <input class="form-control" name="title" id="title" type="hidden" value="<?php echo metadata('item', array('Dublin Core', 'Title')); ?>" />
 				<button class="feedback-button" type="submit"><?php echo __('Send'); ?></button>
 			</form>
 		</div>
 		<div id="feedback-tab"><?php echo __('Feedback'); ?></div>
+        <div id="feedback-thanks"><?php echo __('Thanks for your feedback!'); ?></div>
 	</div>
-
+    <?php endif; ?>
 </div>
 
 <div id="primary">
