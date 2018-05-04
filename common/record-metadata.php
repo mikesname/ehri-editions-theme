a93d557c03123fc2c2e461dc072d74b456a26d17
Mike Bryant <mikesname@gmail.com>
2018-05-04 11:56:41 +0100

    Tweaks to comment form, anchor JS

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -1,3 +1,25 @@
+<script>
+    jQuery(function($) {
+      $("#comment-expand-button").click(function (e) {
+        $("#comment-form").show(400);
+        $("#comment-expand-button").hide(0);
+      });
+
+      var $html = $('html, body');
+      $("a.items-content-button").click(function(e) {
+        var href = $.attr(this, "href");
+        if (href && href.startsWith("#")) {
+          e.preventDefault();
+          $html.animate({
+            scrollTop: $(href).offset().top + 5
+          }, 400, function() {
+            window.location.hash = href;
+          });
+        }
+      })
+    });
+</script>
+
 <?php
     $item = get_view()->{'item'};
     $texts = tei_editions_render_document_texts($item);
@@ -62,5 +84,5 @@
     <h4 id="document-comments"><?php echo __('Comments'); ?></h4>
     <div id="comments-empty" class="comments-empty"><?php echo __('There are no comments yet.'); ?></div>
     <?php CommentingPlugin::showComments(); ?>
-    <div id="comment-expand-button" class="items-content-button"><?php echo __('Comment'); ?></div>
+    <button id="comment-expand-button" class="items-content-button"><?php echo __('Comment'); ?></button>
 <?php endif; ?>
