5ee2ca6debb3512516b65af40ade89e2c9f23470
Mike Bryant <mikesname@gmail.com>
2018-05-22 16:24:43 +0100

    Try and extract JS logic for maps

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,4 +1,4 @@
-jQuery(function($) {
+jQuery(function ($) {
 
   /**
    * If the button's target is visible, set it to
@@ -22,21 +22,20 @@ jQuery(function($) {
 
   // In desktop mode the visibility of the menu
   // is controlled by the element's corresponding
-  // data-class added to the body. When a menu
   // button is clicked, ensure that class is added
   // and other menu button's classes removed.
-  $(".nav-bar-button").click(function(e) {
+  $(".nav-bar-button").click(function (e) {
     e.preventDefault();
     var $this = $(this),
         cls = $this.data("class"),
         $target = $($this.data("target"));
     $body.toggleClass(cls, 400);
-    $(".nav-bar-button").not($this).each(function(i, elem) {
+    $(".nav-bar-button").not($this).each(function (i, elem) {
       $body.removeClass($(elem).data("class"));
     })
   });
-  $(".nav-bar-back").click(function(e) {
-    $(".nav-bar-button").each(function(i, elem) {
+  $(".nav-bar-back").click(function (e) {
+    $(".nav-bar-button").each(function (i, elem) {
       $body.removeClass($(elem).data("class"));
     })
   });
@@ -58,7 +57,7 @@ jQuery(function($) {
   });
 
   // Toggle the facet list expandion when the toggle is clicked.
-  $("#nav-bar-limit-toggle").click(function(e) {
+  $("#nav-bar-limit-toggle").click(function (e) {
     var $this = $(this),
         $target = $($this.data("target")),
         active = $target.is(":visible");
@@ -70,7 +69,7 @@ jQuery(function($) {
 
   // In the unlikely event of resizing the mobile page, reset
   // visibility of some elements that may have been set by JS
-  $(window).resize(function() {
+  $(window).resize(function () {
     if (window.outerWidth > mobileWidth) {
       $(".nav-bar").removeAttr("style");
       $("#nav-bar-limit-search").removeAttr("style");
@@ -80,6 +79,80 @@ jQuery(function($) {
       });
     }
   })
+
+  // Neatline map behaviour
+  var $neatlineMapDefault = 0;
+  var $neatlineMapType = 0;
+  var $neatlineMapTypeResized = 0;
+  $('.neatline-control').each(function () {
+    if ($(this).text() === 'large') {
+      $neatlineMapType = "exhibit-map-large";
+      $neatlineMapTypeResized = "exhibit-map-large-resized";
+    } else if ($(this).text() === 'wide') {
+      $neatlineMapType = "exhibit-map-wide";
+      $neatlineMapTypeResized = "exhibit-map-wide-resized";
+    } else if ($(this).text() === 'default') {
+      $neatlineMapType = "exhibit-map-default";
+      $neatlineMapTypeResized = "exhibit-map-default";
+    }
+  });
+  if ($(".exhibit-block.exhibit-map-default")[0]) {
+    $neatlineMapDefault = 1;
+  } else {
+    $neatlineMapDefault = 0;
+  }
+
+  $("#nav-bar-button-menu").click(function () {
+    if ($("#nav-bar-menu").css("display") == 'none') {
+      if (!$('body').hasClass('search')) {
+        if ($(window).width() > 1180) {
+          if ($neatlineMapDefault == 0) {
+            $("#neatline-map").attr('class', $neatlineMapTypeResized);
+          }
+        }
+      }
+    } else {
+      if ($neatlineMapDefault == 0) {
+        $("#neatline-map").attr('class', $neatlineMapType);
+      }
+    }
+  });
+
+  $("#nav-bar-menu-back").click(function () {
+    if ($neatlineMapDefault == 0) {
+      $("#neatline-map").attr('class', $neatlineMapType);
+    }
+  });
+
+  $("#nav-bar-button-search").click(function () {
+    if ($("#nav-bar-search").css("display") == 'none') {
+      if (!$('body').hasClass('search')) {
+        if ($(window).width() > 1180) {
+          if ($neatlineMapDefault == 0) {
+            $("#neatline-map").attr('class', $neatlineMapTypeResized);
+          }
+        }
+      }
+    } else {
+      if ($neatlineMapDefault == 0) {
+        $("#neatline-map").attr('class', $neatlineMapType);
+      }
+    }
+  });
+
+  $("#nav-bar-search-back").click(function () {
+    if ($neatlineMapDefault == 0) {
+      $("#neatline-map").attr('class', $neatlineMapType);
+    }
+  });
+
+  $(window).resize(function () {
+    if ($(window).width() < 1180) {
+      if ($neatlineMapDefault == 0) {
+        $("#neatline-map").attr('class', $neatlineMapTypeResized);
+      }
+    }
+  });
 
   /* DESKTOP */
   /* DESKTOP:menu */
