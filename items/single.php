428fad194583656ff42c9775f4c4e497c3918196
Mike Bryant <mikesname@gmail.com>
2018-05-30 14:56:00 +0100

    Responsiveness fixes for carousel

--- items/single.php
+++ items/single.php
@@ -1,11 +1,13 @@
 <div class="item record">
-    <a href="<?php echo record_url($item); ?>">
-        <?php echo item_image(null, array('title' => metadata($item, 'display_title')), 0, $item); ?>
-        <div class="item-title-overlay">
-            <div class="item-title">
-                <?php echo metadata($item, 'display_title'); ?>
-            </div>
+    <div class="item-container">
+        <a href="<?php echo record_url($item); ?>">
+            <?php echo item_image(null, array('title' => metadata($item, 'display_title')), 0, $item); ?>
+            <div class="item-title-overlay">
+                <div class="item-title">
+                    <?php echo metadata($item, 'display_title'); ?>
+                </div>
 
-        </div>
-    </a>
+            </div>
+        </a>
+    </div>
 </div>
