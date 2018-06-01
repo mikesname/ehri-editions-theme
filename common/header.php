8d7e621251a4b2880cf2c8d909f1227bb764a010
trivette72 <36375587+trivette72@users.noreply.github.com>
2018-06-01 11:34:20 +0200

    Favicon added

--- common/header.php
+++ common/header.php
@@ -19,6 +19,15 @@
     <?php echo auto_discovery_link_tags(); ?>
 
     <?php fire_plugin_hook('public_head', array('view' => $this)); ?>
+    
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#603cba">
+	<meta name="theme-color" content="#ffffff">
 
     <!-- color schemes -->
     <?php if ($colorScheme = get_theme_option('Color scheme')): ?>
