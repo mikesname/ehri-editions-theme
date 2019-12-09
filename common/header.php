962f41d72105c9c8cd8d5edbcb3ec9e62ea67dd2
Mike Bryant <mikesname@gmail.com>
2019-12-09 13:39:22 +0100

    Add Google Analytics code to the header if an ID is given in config

--- common/header.php
+++ common/header.php
@@ -73,10 +73,23 @@
 		$textTransform = 'font-variant: normal; text-transform:'.$textTransform;
 	}
 	?>
+
     <script>
       // Polyfill CSS variables for older browsers.
       cssVars();
     </script>
+
+    <?php if ($gaid = trim(get_theme_option('ga_code'))): ?>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', '<?php echo $gaid; ?>', 'auto');
+            ga('set', 'anonymizeIp', true);
+            ga('send', 'pageview');
+        </script>
+    <?php endif; ?>
 </head>
 
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
